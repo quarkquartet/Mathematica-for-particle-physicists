118304737bc9cbf8d94e37c3d18a847d8eb7e651
quarkquartet <hepisaacf@gmail.com>
2021-05-14 17:20:17 -0400

    personal setting

--- Kernel/init.m
+++ Kernel/init.m
@@ -1,9 +1,8 @@
 (* ::Package:: *)
 
-(* ::Code::Initialization:: *)
 (** User Mathematica initialization file **)
-Needs["PauliAlgebra`"];
-Needs["LoopIntegrate`"];
+(*Needs["PauliAlgebra`"];*)
+(*Needs["LoopIntegrate`"];*)
 Needs["MatsubaraSum`"];
 Needs["DiagramEditor`"]; 
 Needs["Toolkit`"];
--- install.m
+++ install.m
@@ -6,13 +6,11 @@
 (*FileNameJoin[{$UserBaseDirectory, "SystemFiles", "FrontEnd", "Palettes"}] *)
 
 
-(* ::Code::Initialization:: *)
 FileOverwrite[file1_,file2_]:=
 	(If[FileType[file2]==File,DeleteFile[file2]];
 	CopyFile[file1,file2];)
 
 
-(* ::Code::Initialization:: *)
 DirectoryOverwrite[dir1_,dir2_]:=
 	(If[FileType[dir2]==Directory,DeleteDirectory[dir2,DeleteContents->True]];
 	CopyDirectory[dir1,dir2];)
@@ -22,7 +20,6 @@ DirectoryOverwrite[dir1_,dir2_]:=
 (*Configure global preference. *)
 
 
-(* ::Code::Initialization:: *)
 Module[{sd},
 sd=#->(#1->(Union[#,Capitalize/@#]&@Sort[#2])&@@@OptionValue[Options[$FrontEnd,#],#]&@#)&@SpellingDictionaries;
 FileOverwrite[FileNameJoin[{NotebookDirectory[],"FrontEnd","init.m"}],
@@ -31,7 +28,6 @@ Get[FileNameJoin[{$UserBaseDirectory, "FrontEnd", "init.m"}]];
 SetOptions[$FrontEnd,sd];];
 
 
-(* ::Code::Initialization:: *)
 FileOverwrite[FileNameJoin[{NotebookDirectory[],"Kernel","init.m"}],
 	FileNameJoin[{$UserBaseDirectory, "Kernel", "init.m"}]];
 
@@ -40,7 +36,6 @@ FileOverwrite[FileNameJoin[{NotebookDirectory[],"Kernel","init.m"}],
 (*Install style sheets*)
 
 
-(* ::Code::Initialization:: *)
 FileOverwrite[FileNameJoin[{NotebookDirectory[],"FrontEnd","CMU Article.nb"}],
 	FileNameJoin[{$UserBaseDirectory, 
 		"SystemFiles", "FrontEnd", "StyleSheets","CMU Article.nb"}]];
@@ -50,31 +45,25 @@ FileOverwrite[FileNameJoin[{NotebookDirectory[],"FrontEnd","CMU Article.nb"}],
 (*Install Packages*)
 
 
-(* ::Code::Initialization:: *)
 DirectoryOverwrite[FileNameJoin[{NotebookDirectory[],"MatsubaraSum"}],
 	FileNameJoin[{$InstallationDirectory,"AddOns","ExtraPackages","MatsubaraSum"}]]
 
 
-(* ::Code::Initialization:: *)
 DirectoryOverwrite[FileNameJoin[{NotebookDirectory[],"DiagramEditor"}],
 	FileNameJoin[{$InstallationDirectory,"AddOns","ExtraPackages","DiagramEditor"}]]
 
 
-(* ::Code::Initialization:: *)
-DirectoryOverwrite[FileNameJoin[{NotebookDirectory[],"PauliAlgebra"}],
-	FileNameJoin[{$InstallationDirectory,"AddOns","ExtraPackages","PauliAlgebra"}]]
+(*DirectoryOverwrite[FileNameJoin[{NotebookDirectory[],"PauliAlgebra"}],
+	FileNameJoin[{$InstallationDirectory,"AddOns","ExtraPackages","PauliAlgebra"}]]*)
 
 
-(* ::Code::Initialization:: *)
-DirectoryOverwrite[FileNameJoin[{NotebookDirectory[],"LoopIntegrate"}],
-	FileNameJoin[{$InstallationDirectory,"AddOns","ExtraPackages","LoopIntegrate"}]]
+(*DirectoryOverwrite[FileNameJoin[{NotebookDirectory[],"LoopIntegrate"}],
+	FileNameJoin[{$InstallationDirectory,"AddOns","ExtraPackages","LoopIntegrate"}]]*)
 
 
-(* ::Code::Initialization:: *)
 DirectoryOverwrite[FileNameJoin[{NotebookDirectory[],"Themes"}],
 	FileNameJoin[{$InstallationDirectory,"AddOns","ExtraPackages","Themes"}]]
 
 
-(* ::Code::Initialization:: *)
 DirectoryOverwrite[FileNameJoin[{NotebookDirectory[],"Toolkit"}],
 	FileNameJoin[{$InstallationDirectory,"AddOns","ExtraPackages","Toolkit"}]]
